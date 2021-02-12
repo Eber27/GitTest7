c5bf7b65243795e303a87c59d29867bcaa48045b
Eber27 <eberfreire@gmail.com>
2021-02-12 15:46:16 -0600

    Git test 7 Method

---
+++ GitTest7/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GitTest7
+{
+    class Program
+    {
+        static int Sum(int x, int y)
+            {
+                return x + y;
+            }
+            static void Main(string[] args)
+            {
+                Console.WriteLine(Sum(8, 6));
+            }
+    }
+}
